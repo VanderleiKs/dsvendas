01067a0bb7672fb93244a28894234d350d6ccc95
Vanderlei K Silva <vanderleis47@gmail.com>
2021-05-04 20:44:51 -0300

    add bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <h1>Ola mundo</h1>
+      <h1 className="text-primary">Ola mundo</h1>
+      <button className="btn btn-primary">Clique aqui</button>
     </div>
   );
 }
