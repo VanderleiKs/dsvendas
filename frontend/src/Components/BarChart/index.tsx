4aaefb114277bf439e9f8dabcd108b2fa1fdaba6
Vanderlei K Silva <vanderleis47@gmail.com>
2021-05-08 19:38:12 -0300

    BarChart integration

--- frontend/src/Components/BarChart/index.tsx
+++ frontend/src/Components/BarChart/index.tsx
@@ -1,33 +1,49 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
 
 const BarChart = () => {
+    const[chartData, setChartData] = useState<ChartData>({labels: {categories: []}, series:[]});
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/successbyseller`)
+        .then(response => {
+            const data = response.data as SaleSuccess[];
+            const dataSuccess = data.map(x => round(((x.deals / x.visited) * 100), 1));
+            const dataCategory = data.map(x => x.sellerName);
+            setChartData({labels:{categories: dataCategory}, series:[{name: "% Success", data: dataSuccess}]});
+        })
+    }, []);
 
     const options = {
         plotOptions: {
             bar: {
-                horizontal: true,
-                
+                horizontal: true,       
             }
         },
-        colors: ['#03c8af']
-       
+        colors: ['#03c8af']   
     };
 
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
     return (
         <Chart
-            options={{ ...options, xaxis: mockData.labels}}
-            series={mockData.series}
+            options={{ ...options, xaxis: chartData.labels}}
+            series={chartData.series}
             type='bar'
             height='240'
         />
--- frontend/src/Components/DonutChart/index.tsx
+++ frontend/src/Components/DonutChart/index.tsx
@@ -23,13 +23,6 @@ const DonutChart = () => {
         });
     },[]);
 
-
-    /*const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: chart
-    }
-    */
-
     const options = {
         legend: {
             show: true
@@ -38,7 +31,8 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{ ...options, labels: chartData.labels}} series={chartData.series} type='donut' height='240'
+            options={{ ...options, labels: chartData.labels}} 
+            series={chartData.series} type='donut' height='240'
         />
     )
 }
