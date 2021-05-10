c62f657debd97fbf2e054804271a7574f37d9187
Vanderlei K Silva <vanderleis47@gmail.com>
2021-05-10 18:23:34 -0300

    fixed errors

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
                     <BarChart />
                 </div>
                 <div className="col-sm-6">
-                    <h5 className="text-center text-secundary">Participação nas venda</h5>
+                    <h5 className="text-center text-secundary">Participação nas vendas</h5>
                     <DonutChart />
                 </div>
             </div>
